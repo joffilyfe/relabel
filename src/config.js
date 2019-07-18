c08f62a5776077c20c9c8180f86708bdada92a34
Joffily Ferreira <joffily.ferreira@scielo.org>
2019-07-18 18:09:19 -0300

    add default labels

--- src/config.js
+++ src/config.js
@@ -26,13 +26,53 @@ const table = {
     name: 'enhancement',
     color: 'a2eeef',
     description: 'New feature or request'
+  },
+  duplicate: {
+    name: 'duplicate',
+    color: 'cfd3d7',
+    description: 'This issue or pull request already exists'
+  },
+  'good first issue': {
+    name: 'good first issue',
+    color: '7057ff',
+    description: 'Good for newcomers'
+  },
+  'help wanted': {
+    name: 'help wanted',
+    color: '008672',
+    description: 'Extra attention is needed'
+  },
+  invalid: {
+    name: 'invalid',
+    color: 'e4e669',
+    description: "This doesn't seem right",
+  },
+  question: {
+    name: 'question',
+    color: 'd876e3',
+    description: 'Further information is requested'
+  },
+  wontfix: {
+    name: 'wontfix',
+    color: 'ffffff',
+    description: 'This will not be worked on'
+  },
+  task: {
+    name: 'task',
+    color: 'cc9949',
+    description: 'A minimal piece of work.'
   }
 }
 
 const renameTable = {
   erro: table.bug,
   melhoria: table.enhancement,
-  'feature-request': table.enhancement
+  'feature-request': table.enhancement,
+  duplicado: table.duplicate,
+  impedido: table.wontfix,
+  'inválido': table.invalid,
+  'discussão': table["help wanted"],
+  bug: table.bug
 }
 
 const changeLabelsTable = renameTable
