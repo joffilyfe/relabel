73b1e71cd0e4dfd0832432642f066309a67239f2
Joffily Ferreira <joffily.ferreira@scielo.org>
2019-07-19 14:18:19 -0300

    add token options to issue listing

--- bin/relabel-issues-list.js
+++ bin/relabel-issues-list.js
@@ -5,10 +5,15 @@ const { fetchIssues } = require('../src/issues')
 program
   .option('-r, --repo <name>', 'Set the repository where actions will be performed')
   .option('-o, --owner <name>', 'Set the repository owner')
+  .option('-t, --token <personal github token>', 'Set your personal github token')
   .parse(process.argv)
   // TODO: add support for filters
 
 const options = program.opts()
+const headers = {
+  Accept: 'application/vnd.github.symmetra-preview+json',
+  'Content-Type': 'application/json'
+}
 
 if (!options.repo || !options.owner) {
   console.error('Error: a repository and owner is required')
@@ -16,7 +21,9 @@ if (!options.repo || !options.owner) {
   process.exit(1)
 }
 
-fetchIssues({ owner: options.owner, repo: options.repo })
+options.token = options.token || process.env.token
+
+fetchIssues({ owner: options.owner, repo: options.repo, token: options.token })
   .then(data => {
     data.issues.forEach(issue => {
       console.log(`#${issue.number} - ${issue.title} - ${issue.url}`)
